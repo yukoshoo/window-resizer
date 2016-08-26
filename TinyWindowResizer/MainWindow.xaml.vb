d922c6c68308ddc8bade9755e603e0bac083ab7f
yukoshoo <yukoshoo@gmail.com>
2016-08-25 17:36:20 -0700

    GetWindowRect working. Optional window movement on resize with checkbox.

--- TinyWindowResizer/MainWindow.xaml.vb
+++ TinyWindowResizer/MainWindow.xaml.vb
@@ -4,33 +4,39 @@ Class MainWindow
 
     Public Declare Function MoveWindow Lib "user32" (ByVal hwnd As Int32, ByVal x As Int32, ByVal y As Int32, ByVal nWidth As Int32, ByVal nHeight As Int32, ByVal bRepaint As Boolean) As Boolean
     Declare Auto Function FindWindow Lib "user32" (ByVal lpClassName As String, ByVal lpWindowName As String) As IntPtr
+    Private Declare Function GetWindowRect Lib "user32" (ByVal hWnd As IntPtr, ByRef lpRect As RECT) As Integer
 
-    ' Private Declare Function GetWindowRect Lib "user32" (ByVal hWnd As Long, lpRect As RECT) As Long
-
-    ' Private Structure RECT
-    ' Public left, top, right, bottom As Integer
-    ' End Structure
+    Private Structure RECT
+        Dim left, top, right, bottom As Integer
+    End Structure
 
     Private Sub bBorderless_Click(sender As Object, e As RoutedEventArgs) Handles bBorderless.Click
         Dim wHandle As IntPtr = FindWindow(Nothing, tNList.SelectedItem)
-        '  Dim wr As RECT
-        '  GetWindowRect(wHandle, wr)
-        '  Dim topleft = wr.left.ToString
-        ' Dim toptop = wr.top.ToString
+        Dim wr As RECT
+        GetWindowRect(wHandle, wr)
         Dim x As Integer = tWidth.Text
         Dim y As Integer = tHeight.Text
-        MoveWindow(wHandle, topleft, toptop, x, y, True)
+        If coordbox.IsChecked Then
+            MoveWindow(wHandle, wr.left.ToString, wr.top.ToString, x, y, True)
+        Else
+            MoveWindow(wHandle, 0, 0, x, y, True)
+        End If
+        'MoveWindow(wHandle, 0, 0, x, y, True)
     End Sub
 
     Private Sub bBorder_Click(sender As Object, e As RoutedEventArgs) Handles bBorder.Click
         Dim wHandle As IntPtr = FindWindow(Nothing, tNList.SelectedItem)
-        '  Dim wr As New RECT
-        '  GetWindowRect(wHandle, wr)
-        '  Dim topleft = wr.left.ToString
-        '  Dim toptop = wr.top.ToString
+        Dim wr As RECT
+        GetWindowRect(wHandle, wr)
         Dim x As Integer = tWidth.Text
         Dim y As Integer = tHeight.Text
-        MessageBox.Show(wr.left.ToString)
+        If coordbox.IsChecked Then
+            MessageBox.Show("whandle, wr.Left.ToString, wr.top.ToString, x, y, True")
+        Else
+            MessageBox.Show("whandle, 0, 0, x, y, True")
+        End If
+        'MessageBox.Show(wr.left.ToString)
+        'MessageBox.Show(wHandle)
     End Sub
 
     Private Sub tNList_DropDownOpened(sender As Object, e As EventArgs) Handles tNList.DropDownOpened
@@ -46,6 +52,10 @@ Class MainWindow
                 End If
             End If
         Next
+    End Sub
+
+    Private Sub coordbox_Checked(sender As Object, e As RoutedEventArgs) Handles coordbox.Checked
+
     End Sub
 End Class
 
